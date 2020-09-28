54af346012e9adb34ff7b8a24660bfc1c20118a9
Whan Jung <wjun856>
2020-09-28 21:07:23 +1300

    Added source folder that contains simple template of quinzical.Main class

---
+++ src/quinzical/Main.java
@@ -0,0 +1,29 @@
+package quinzical;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void main(String[] args) {
+		launch(args);		
+	}
+}
