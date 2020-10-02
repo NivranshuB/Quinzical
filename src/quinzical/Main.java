17069d693cddfc4daf82643bd2dc03298fd6ea21
Whan Jung <wjun856>
2020-10-03 12:15:27 +1300

    Added functionality for practice module button

--- src/quinzical/Main.java
+++ src/quinzical/Main.java
@@ -34,6 +34,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		
+		_questions = new QuestionRetriever();//initialise categories and questions
+		_gameWindow = primaryStage;
 		primaryStage.setTitle("Quinzical!");
 
 		StackPane menuTitleText = new StackPane();
@@ -56,18 +58,14 @@ public class Main extends Application {
 		gamesModuleButton.setStyle("-fx-border-color: #200459;-fx-border-width: 1;-fx-font-size: 16;");
 		gamesModuleButton.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle (ActionEvent e) {
-				//gamesModule(primaryStage);
+				//gamesModule(_gameWindow);
 			}
 		});
 		
 		Button practiceModuleButton = new Button("Practice Module");
 		practiceModuleButton.setPrefSize(460,60);
 		practiceModuleButton.setStyle("-fx-border-color: #070459;-fx-border-width: 1;-fx-font-size: 16;");
-		practiceModuleButton.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle (ActionEvent e) {
-				practiceModule(primaryStage);
-			}
-		});
+		
 		
 		Button exitButton = new Button("Exit Game");
 		exitButton.setPrefSize(460,60);
@@ -77,7 +75,8 @@ public class Main extends Application {
 				boolean confirmation = ConfirmBox.displayConfirm("Exit confirmation", "Are you sure "
 						+ "you want to exit? (Don't worry your progress will be saved)");
 				if (confirmation) {
-					exitGame();
+					_gameWindow.close();
+//					exitGame();
 				}
 			}
 		});
@@ -88,9 +87,14 @@ public class Main extends Application {
 		menuLayout.setPadding(new Insets(20, 20, 30, 20)); 
 		menuLayout.getChildren().addAll(menuTitleText, menuInfo, gamesModuleButton, practiceModuleButton, exitButton);
 
-		Scene menuScene = new Scene(menuLayout, 700, 450);
-		
-		primaryStage.setScene(menuScene);
+		_menuScene = new Scene(menuLayout, 700, 450);
+		practiceModuleButton.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle (ActionEvent e) {
+				PracticeModule module = new PracticeModule(_gameWindow, _questions, _menuScene);
+				module.practiceModule();
+			}
+		});
+		primaryStage.setScene(_menuScene);
 		primaryStage.show();
 		
 		
@@ -98,39 +102,39 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);		
 	}
-	private void exitGame() {
-
-		String save_loc = System.getProperty("user.dir") + System.getProperty("file.separator") + "save_data";
-		Path pathCategoryData = Paths.get(save_loc + System.getProperty("file.separator") + "categories");
-
-		try {
-			Files.createDirectories(pathCategoryData);
-		} catch (IOException e) {
-			System.err.println("Failed to create directory!" + e.getMessage());
-		}
-
+//	private void exitGame() {
+//
+//		String save_loc = System.getProperty("user.dir") + System.getProperty("file.separator") + "save_data";
+//		Path pathCategoryData = Paths.get(save_loc + System.getProperty("file.separator") + "categories");
+//
 //		try {
-//			FileWriter winningsWriter = new FileWriter(save_loc + "/winnings");
-//			winningsWriter.write(String.valueOf((_winnings)));
-//			winningsWriter.close();
+//			Files.createDirectories(pathCategoryData);
 //		} catch (IOException e) {
-//			e.printStackTrace();
+//			System.err.println("Failed to create directory!" + e.getMessage());
 //		}
-
-		//Write data for each question to its respective category file
-		for (Category c : _questions.getCategoryList()) {
-			try {
-				FileWriter categoryWriter = new FileWriter(save_loc + "/categories/" + c.getCategoryName());
-
-				for (Question q : c.getQuestions()) {
-					categoryWriter.write(q.getValue() + "," + q.getQuestion() + "," + q.getAnswer() + "\n");
-				}				
-				categoryWriter.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		_gameWindow.close();
-	}
+//
+////		try {
+////			FileWriter winningsWriter = new FileWriter(save_loc + "/winnings");
+////			winningsWriter.write(String.valueOf((_winnings)));
+////			winningsWriter.close();
+////		} catch (IOException e) {
+////			e.printStackTrace();
+////		}
+//
+//		//Write data for each question to its respective category file
+//		for (Category c : _questions.getCategoryList()) {
+//			try {
+//				FileWriter categoryWriter = new FileWriter(save_loc + "/categories/" + c.getCategoryName());
+//
+//				for (Question q : c.getQuestions()) {
+//					categoryWriter.write(q.getQuestion() + "," + q.getClue() + "," + q.getAnswer() + "\n");
+//				}				
+//				categoryWriter.close();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//
+//		_gameWindow.close();
+//	}
 }
