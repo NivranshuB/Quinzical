1c747e4a6086b6094aa94d7d04461b928d2e7216
Whan Jung <wjun856>
2020-10-03 19:28:02 +1300

    Minor changes to AlertBox class

--- src/quinzical/AlertBox.java
+++ src/quinzical/AlertBox.java
@@ -30,6 +30,10 @@ public class AlertBox {
 	 * @param color | The color of the text message of the alert box
 	 */
 	public static void displayAlert(String title, String message, String color) {
+		
+		HelperThread helper = new HelperThread(message);
+		helper.start();
+		
 		Stage window = new Stage();
 		
 		window.initModality(Modality.APPLICATION_MODAL);
